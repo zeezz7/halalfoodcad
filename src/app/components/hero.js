a6b5092e0733cedc385f99b67fa391f1bc06d9c2
aziz <coding2537@gmail.com>
2025-03-05 00:31:46 +0530

    responsive

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -4,10 +4,10 @@ import Image from "next/image";
 
 function hero() {
   return (
-    <div className="pt-4 m-auto mt-[-30px] flex flex-col md:mt-[8px] md:flex-row w-[90%] md:w-[75%] min-h-[80vh] md:h-[90vh] justify-center items-center font-sika">
-      <div className="flex flex-col h-1/2 justify-between w-full">
+    <div className="pt-4 m-auto mt-[-30px] flex flex-col md:mt-[8px] md:flex-row w-[90%] md:w-[85%] min-h-[80vh] md:h-[90vh] justify-center items-center  font-sika">
+      <div className="flex flex-col h-1/2 justify-between w-full lg:w-[90%]">
         <div className="flex flex-col space-y-4 md:space-y-7">
-          <div className="text-[#1B3B31] tracking-normal text-xl sm:text-[22px] md:text-3xl lg:text-4xl font-bold text-center md:text-left pt-5 md:pt-0">
+          <div className="text-[#1B3B31] tracking-normal text-[24px] sm:text-xl md:text-3xl lg:text-4xl font-bold text-center md:text-left pt-5 md:pt-0">
             Delivering Convenience, Ensuring{" "}
             <span className="italic font-bold">Halal Food</span>
           </div>
@@ -20,22 +20,25 @@ function hero() {
           <div className="italic text-xs sm:text-sm md:text-base lg:text-lg tracking-wide text-[#232323] text-center md:text-left">
             Join the waitlist and be the first to know when we launch!
           </div>
-          <div className="border-2 flex flex-col sm:flex-row gap-2 sm:gap-0 border-[#9CACA7] rounded-[8px] w-full pr-2 pl-3 py-1">
+          <div className="border-2 flex  justify-between flex-row   sm:gap-0 border-[#9CACA7] rounded-[8px] w-full pr-1 pl-3 py-1">
             <input
               placeholder="Enter your email to stay updated"
               className="border-none focus:outline-none w-full sm:w-3/4 text-xs sm:text-sm md:text-base p-1"
             />
-            <div className="w-full sm:w-auto">
-              <Button value="Notify Me" />
+            <div className=" w-[180px]    ">
+              <Button
+                value="Notify Me"
+                customWidth=" w-[180px]  md:w-[180px]"
+              />
             </div>
           </div>
         </div>
       </div>
-      <div className="w-full md:w-[70%] select-none mt-8 md:mt-0">
+      <div className="w-full md:w-[60%] mr-0 lg:mr-[-50px] select-none mt-8 md:mt-0">
         <Image
           priority
-          width={500}
-          height={500}
+          width={400}
+          height={400}
           src="Group2.svg"
           alt="Group2"
           quality={75}
--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 function nav() {
   return (
     <nav className="w-full">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-6 lg:py-8">
+      <div className=" mx-auto px-4 sm:px-6 lg:px-28 py-4 sm:py-6 lg:py-8">
         <Link href="/">
           <div className="text-[#607F72] text-lg sm:text-xl lg:text-[22px] font-semibold select-none cursor-pointer hover:text-[#4a665b] transition-colors">
             HALAL GOES
